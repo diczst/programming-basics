c1b566c086bc14e90fd02cef4b56d6d2e25bbfaf
dickyzuerst <dzuerst@gmail.com>
2024-11-10 20:27:58 +0700

    add main.go

---
+++ go/fundamentals/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var namaDepan = "Dicky"
+	var namaBelakang = "Pratama"
+	var umur = 99
+
+	fmt.Println("Hello world : " + namaDepan + " " + namaBelakang)
+	fmt.Println("Umur : " + strconv.Itoa(umur))
+}
