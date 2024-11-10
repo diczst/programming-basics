6fe932ea49a10e9c05e5f2eca777559b59a1c668
dickyzuerst <dzuerst@gmail.com>
2024-11-10 20:31:45 +0700

    add go

--- go/fundamentals/main.go
+++ go/fundamentals/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strconv" // untuk mengkonversi int to string
 )
 
 func main() {
-	var namaDepan = "Dicky"
-	var namaBelakang = "Pratama"
-	var umur = 99
+	var negara string = "Indonesia" // deklarasi variabel dengan tipe data
+	var namaDepan = "Dicky"         // deklarasi variabel tanpa tipe data
+	var umur = 99                   // deklarasi variabel dengan nilai string
 
-	fmt.Println("Hello world : " + namaDepan + " " + namaBelakang)
+	hobi := "membaca" // := adalah svd (Short Variable Declaration)  hanya bisa digunakan di dalam fungsi dan tujuannya untuk mempersingkat deklarasi variabel saja
+
+	fmt.Println("Hello world : " + namaDepan)
 	fmt.Println("Umur : " + strconv.Itoa(umur))
+	fmt.Println("Hobi : " + hobi)
+	fmt.Println("Negara : " + negara)
 }
